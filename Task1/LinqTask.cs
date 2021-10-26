98c7524bf468c507f889b4b9a07ae08d95f3b810
Maxim Mochalkin <maksimmochalkin@gmail.com>
2021-10-26 14:19:50 +0600

    saome changes

--- Task1/LinqTask.cs
+++ Task1/LinqTask.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Reflection;
 using Task1.DoNotChange;
 
 namespace Task1
@@ -17,83 +18,37 @@ namespace Task1
                 select customer);
         }
 
-        public static Dictionary<Customer, List<Supplier>> Linq2(
+        public static IEnumerable<(Customer, IEnumerable<Supplier>)> Linq2(
             IEnumerable<Customer> customers,
             IEnumerable<Supplier> suppliers
         )
         {
-            var tt = customers.Where((customer) => suppliers
-                .Contains(supplier => customer.City == supplier.City && customer.Country == supplier.Country))
+            var result = customers.Select(c => (c, suppliers
+                .Where(s => s.City == c.City && s.Country == c.Country).Select(s => s))).ToList();
 
-
-            var dictionary = (from customer in customers
-                from supplier in suppliers
-                where customer.City == supplier.City && customer.Country == supplier.Country
-                select new
-                {
-                   Customer = new Customer
-                   {
-                       CustomerID = customer.CustomerID,
-                       City = customer.City,
-                       Country = customer.Country
-                   },
-                   Supliers = new List<Supplier>
-                   {
-                       new Supplier
-                       {
-                           City = supplier.City,
-                           Country = supplier.Country
-                       }
-                   }
-
-                }).ToDictionary(item => item.Customer, item => item.Supliers);
-
-            return dictionary;
+            return result;
         }
 
-        public static Dictionary<Customer, List<Supplier>> Linq2UsingGroup(
+        public static IEnumerable<(Customer, IEnumerable<Supplier>)> Linq2UsingGroup(
             IEnumerable<Customer> customers,
             IEnumerable<Supplier> suppliers
         )
         {
-            // need to talk
-            var dictionary = (from customer in customers
-                from supplier in suppliers
-                where customer.City == supplier.City && customer.Country == supplier.Country
-                select new
-                {
-                    Customer = new Customer
-                    {
-                        CustomerID = customer.CustomerID,
-                        City = customer.City,
-                        Country = customer.Country
-                    },
-                    Supliers = new List<Supplier>
-                    {
-                        new Supplier
-                        {
-                            City = supplier.City,
-                            Country = supplier.Country
-                        }
-                    }
+            var result = customers.Select(c => (c, suppliers
+                .Where(s => s.City == c.City && s.Country == c.Country).Select(s => s))).ToList();
 
-                }).ToDictionary(item => item.Customer, item => item.Supliers);
 
-            return dictionary;
+            return result;
         }
 
         public static IEnumerable<Customer> Linq3(IEnumerable<Customer> customers, decimal limit)
         {
-            return (from customer in customers
-                let total = customer.Orders.Sum(order => order.Total)
-                where total > limit
-                select customer);
-            //var listCustomers = (from customer in customers 
-            //    from order in customer.Orders 
-            //    where order.Total > limit 
-            //    select customer).ToList();
+            var listCustomers = (from customer in customers
+                                 from order in customer.Orders
+                                 where order.Total > limit
+                                 select customer).Distinct().ToList();
 
-            //return listCustomers;
+            return listCustomers;
 
         }
 
@@ -101,34 +56,46 @@ namespace Task1
             IEnumerable<Customer> customers
         )
         {
-            var tt = (from customer in customers
-                from order in customer.Orders
-                where order.OrderDate.Year > DateTime.Parse("1995-01-01T00:00:00").Year
-                      select customer).Distinct().ToList();
+            var filteredCustomers = (from customer in customers
+                                    where customer.Orders.Count() != 0
+                                    select customer).ToList();
 
-                //select new
-                //{
-                //    Customer = new Customer
-                //    {
-                //        CustomerID = customer.CustomerID,
-                //        CompanyName = customer.CompanyName
-                //    },
-                //    DateOfEntry = order.OrderDate
-                //}).ToList();
+            var customersWithDateOfEntry = (from customer in filteredCustomers
+                      let dateOfEntry = customer.Orders.Select(d => d.OrderDate).Min()
+                      select (customer, dateOfEntry)).Distinct().ToList();
 
-                 throw new NotImplementedException();
+            return customersWithDateOfEntry;
         }
 
         public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq5(
             IEnumerable<Customer> customers
         )
         {
-            throw new NotImplementedException();
+            var filteredCustomers = (from customer in customers
+                                     where customer.Orders.Count() != 0
+                                     select customer).ToList();
+
+            var customersWithDateOfEntry = (from customer in filteredCustomers
+                                            let dateOfEntry = customer.Orders.Select(d => d.OrderDate)
+                                            select (customer, dateOfEntry.Min())).Distinct().ToList();
+
+            var sortedCustomers = (from customer in customersWithDateOfEntry
+                                   orderby customer.Item2.Year, customer.Item2.Month, customer.Item2.Day ascending
+                                   select (customer)).ToList();
+
+
+
+            return sortedCustomers;
         }
 
         public static IEnumerable<Customer> Linq6(IEnumerable<Customer> customers)
         {
-            throw new NotImplementedException();
+            var filteredCustomers = (from customer in customers
+                                     where customer.PostalCode.Any(char.IsLetter) ||
+                                     customer.Region ==  null || customer.Phone[0] != '('
+                                     select customer).ToList();
+
+            return filteredCustomers;
         }
 
         public static IEnumerable<Linq7CategoryGroup> Linq7(IEnumerable<Product> products)
@@ -144,7 +111,20 @@ namespace Task1
 		            price - 19.0000
              */
 
-            throw new NotImplementedException();
+            var result = products.GroupBy(cat => cat.Category)
+                .Select(c => new Linq7CategoryGroup
+                {
+                    Category = c.Key,
+                    UnitsInStockGroup = c.GroupBy(uis => uis.UnitsInStock)
+                        .Select(u => new Linq7UnitsInStockGroup
+                        {
+                            UnitsInStock = u.Key,
+                            Prices = u.GroupBy(up => up.UnitPrice)
+                                .Select(p => (p.Key)).ToList()
+                        }).ToList()
+                }).ToList();
+           
+            return result;
         }
 
         public static IEnumerable<(decimal category, IEnumerable<Product> products)> Linq8(
@@ -154,6 +134,27 @@ namespace Task1
             decimal expensive
         )
         {
+            var cheapList = products.Select(c => new
+                {
+                    Category = cheap,
+                    Products = products.Where(p => p.UnitPrice == products.Select(p => p.UnitPrice).Min()).ToList()
+            }).Select(c => (c.Category, c.Products)).ToList();
+
+            var middleList = products.Select(c => new
+            {
+                Category = middle,
+                Products = products.Where(p => p.UnitPrice != products.Select(p => p.UnitPrice).Min()
+                && p.UnitPrice != products.Select(p => p.UnitPrice).Max()).ToList()
+            }).Select(c => (c.Category, c.Products)).ToList();
+
+            var expensiveList = products.Select(c => new
+            {
+                Category = expensive,
+                Products = products.Where(p => p.UnitPrice == products.Select(p => p.UnitPrice).Max()).ToList()
+            }).Select(c => (c.Category, c.Products)).ToList();
+
+            var unionResult = cheapList.Concat(middleList).Concat(expensiveList).ToList();
+
             throw new NotImplementedException();
         }
 
@@ -161,7 +162,15 @@ namespace Task1
             IEnumerable<Customer> customers
         )
         {
-            throw new NotImplementedException();
+            var result = customers.Where(co => co.Orders.Count() != 0)
+                .Select(c => new
+                {
+                    City = c.City,
+                    AverageIncome = (int)c.Orders.Average(o => o.Total),
+                    AverageIntensity = (int)customers.Average(count => count.Orders.Count())
+                }).Select(res => (res.City, res.AverageIncome, res.AverageIntensity)).ToList();
+
+            return result;
         }
 
         public static string Linq10(IEnumerable<Supplier> suppliers)
